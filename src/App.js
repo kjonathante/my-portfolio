a66292420c7784c6dd03cd5c7976008234f42591
Kit Jonathan Te <kjonathante@gmail.com>
2018-11-21 15:24:16 -0800

    Create GridContainer and GridItem components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import React, { Component } from "react";
 import Parallax from "./components/Parallax/Parallax";
 import Container from "./components/Container";
 import Main from "./components/Main/Main";
+import GridContainer from "./components/Grid/GridContainer";
+import GridItem from "./components/Grid/GridItem";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Parallax filter image={require("./assets/img/dark_background.jpg")}>
-          <Container style={{ color: "white" }}>
-            <h1>Kit Jonathan Te</h1>
+          <Container>
+            <GridContainer>
+              <GridItem xs={12} md={6}>
+                <h1>Kit Jonathan Te</h1>
+                <h4>
+                  Every landing page needs a small description after the big
+                  bold title, that's why we added this text here. Add here all
+                  the information that can make you or your product create the
+                  first impression.
+                </h4>
+              </GridItem>
+            </GridContainer>
           </Container>
         </Parallax>
         <Main>
